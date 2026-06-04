aab6f86dc65cae3706c0c7b32b13c0d86d8062f0
Aman Raj <amanraj3704@gmail.com>
2026-06-04 16:43:31 +0530

    Prepare for deployment

--- backend/src/main/java/com/cfs/TechCodesolution/TechCodesolutionApplication.java
+++ backend/src/main/java/com/cfs/TechCodesolution/TechCodesolutionApplication.java
@@ -3,6 +3,7 @@ package com.cfs.TechCodesolution;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class TechCodesolutionApplication {
